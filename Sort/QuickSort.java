55873d5b73ad1e70de0404ab9ec5c81c87477aa8
Constantine Dimitrov <konydim@gmail.com>
2018-09-29 20:09:44 +0100

    FIX: All bugs removed.
    The algorithm now works perfectly.

--- Sort/QuickSort.java
+++ Sort/QuickSort.java
@@ -6,16 +6,63 @@ public class QuickSort {
 
 	private String[] stringInput = null;
 	private int[] arrayToSort = null;
+	private int pivot, partitionPoint,
+						 	leftIndex, rightIndex;
+	private int minAssertedIndex; // The value of .
+
+  private static int breaker=0;
 
 	public QuickSort(String rawInput) 
 	{
 		stringInput = rawInput.split(" ");
 		arrayToSort = new int[stringInput.length];
 
-		for (int i=0; i<stringInput.length; i++) 
-		{
+		for (int i=0; i<stringInput.length; i++)
 			arrayToSort[i] = Integer.parseInt(stringInput[i]);
+
+		// The scanning indices
+		leftIndex = 1;	// Looks for greater than pivot
+		rightIndex = arrayToSort.length-1;
+	}
+
+	private void swapValues(int indexA, int indexB) {
+		int temp;
+
+		temp = arrayToSort[indexB];
+		arrayToSort[indexB] = arrayToSort[indexA];
+		arrayToSort[indexA] = temp;
+	} 
+
+	public int partition_lomuto(int leftIndex, int rightIndex) {
+
+		pivot = arrayToSort[rightIndex];
+		minAssertedIndex = leftIndex;
+
+		for (int j=leftIndex; j<=rightIndex-1; j++) {
+			if (arrayToSort[j] <= pivot) {
+				swapValues(minAssertedIndex, j);
+				minAssertedIndex++;
+			}
 		}
+		swapValues(minAssertedIndex, rightIndex); // rightIndex - the index of the pivot
+
+		return minAssertedIndex;
+	}
+
+	public void quickSort(int start, int end) {
+		
+		if (start < end) {
+			partitionPoint = partition_lomuto(start, end);
+			quickSort(start, partitionPoint-1);	// On left of the pivot.
+			quickSort(partitionPoint, end);	// On right of the pivot.
+		}
+	}
+
+	public void printArray() {
+		for (int i=0; i<arrayToSort.length; i++) {
+			System.out.print(arrayToSort[i] + " ");
+		}
+		System.out.print("\n");
 	}
 
 
@@ -36,6 +83,9 @@ public class QuickSort {
 			e.printStackTrace();
 		}
 
+		// Sort the arrayToSort
+		qs.quickSort(0, qs.arrayToSort.length-1);
 
+		qs.printArray();
 	}
 }
